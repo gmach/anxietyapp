d05b4c9fa32b0f4cd65b3e363be8d80f999b3052
Gavin Mach <gavinmach@gmail.com>
2022-12-09 13:40:21 +1100

    fix build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <main>
+        <h1>Situation</h1>
+<p>
+The end of year exams are approaching.
+</p>
+<h1>Emotion</h1>
+<p>
+Nervous, stressed and apprehensive. 
+</p>
+<h1>Unhelpful Thought</h1>
+<p>
+I'll definitely fail my exams miserably!
+</p>
+
+<h1>Challenges To Unhelpful Thought</h1>
+
+<h2>Is there any evidence that contradicts this thought?</h2>
+<p>
+ I've always done well in my previous exams.
+ I've been scoring well in my coursework.
+</p>
+<h2>Can you identify any of the patterns of unhelpful thinking?</h2>
+<p>
+Predicting the Future:
+When we are feeling anxious, it is common for us to spend a lot of time thinking about the future and predicting what could go wrong, rather than just letting things be. In the end most of our predictions don't happen and we have wasted time and energy being worried and upset about them.
+For example:
+ Assuming you will perform poorly at your job interview.
+ Spending the week before an exam predicting you will
+fail, despite all your hard work studying and your previous good grades.
+This means that you make assumptions about others' beliefs without having any real evidence to support them.
+For example:
+ My boss thinks I'm stupid.
+ People think I'm weird.
+Such ways of thinking naturally make us apprehensive.
+People commonly 'catastrophise' when they are anxious, which basically means that they often blow things out of proportion.
+For example:
+ They assume that something that has happened is far
+worse than it really is (e.g. that their friend is going to
+dislike them because they cancelled a night out).
+ They may think that something terrible is going to
+happen in the future, when, in reality, there is very little evidence to support it (e.g. I'm going to get into serious trouble for calling in sick).
+Anxious people often have a tendency to focus on the negatives which keeps their anxiety going.
+For example:
+ They focus on the one person at work who doesn't
+like them, ignoring that they are very popular with the rest of their colleagues.
+Mind Reading:
+Catastrophising:
+Focusing on the Negatives:
+Should Statements:
+People often imagine how they would like things to be or how they 'should be' rather than accepting how things really are.
+For example:
+ I should have got an A in History.
+ I should never be anxious.
+Unfortunately when we do this, we are simply applying extra pressure to ourselves that can result in anxiety. Instead it can sometimes help to accept that things can't always be perfect.
+Based on one isolated incident you assume that all others will follow a similar pattern in the future.
+For example:
+ When enrolling on a college course, you meet a
+future classmate who you find irritating. As a result, you worry that everyone in the class will be the same and you won't make any friends.
+Have you ever wondered "what if" something bad happens?
+For example
+ What if I have a panic attack at the party?
+ What if I don't make friends when I start my new
+job?
+This type of thought can often make us avoid going places or doing the things that we would like.
+Do you find that you attach negative labels to yourself?
+For example:
+ I'm weak.
+ I'm a waste of space.
+ I'm always anxious.
+</p>
+<p>
+ I'm 'predicting the future'. I have no evidence to suggest I'll fail.
+</p>
+<h2>What would you say to a friend who had this thought in a similar situation?</h2>
+<p>
+ I'd say don't be silly, you've always done well. As long as you've studied hard, you should be fine. Besides, you can only try your best.
+</p>
+<h2>What are the costs and benefits of thinking in this way?</h2>
+<p>
+ Costs: It's making me feel sick with worry.
+ Benefits: I can't really think of any.
+</p>
+<h2>How will you feel about this in 6 months time?</h2>
+<p> I'll probably look back and laugh about how silly I was being.
+</p>
+<h2>Is there another way of looking at this situation?</h2>
+<p>
+   I've always done well in the past so I should be ok. I can only do my best anyway; after all I've studied hard. At worst, I'll just have to re-sit next year.
+</p>
+<h1>Balanced Thought</h1>
+<p>
+Worrying about failing is doing me no good. I've always done well before so I should be fine, especially since I've prepared properly.
+</p>
+      </main>
     </div>
   );
 }
